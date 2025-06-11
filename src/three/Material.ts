a558ee3c37cab2569ac16a4fc92f84afa07092ae
Ulysses Popple <codes@ulysses.me>
2025-06-11 08:56:18 +0100

    add material fns

--- src/three/Material.ts
+++ src/three/Material.ts
@@ -216,5 +216,492 @@ export function dispose(): void {
 
 export function setNeedsUpdate(value: boolean): void {
   // Implementation would go here
-// Export all functions for use in other modules
+}
+
+// Setter functions for Material properties
+
+export function alphaHash(material: any, value: boolean): void {
+  material.alphaHash = value;
+}
+
+export function alphaTest(material: any, value: number): void {
+  material.alphaTest = value;
+}
+
+export function alphaToCoverage(material: any, value: boolean): void {
+  material.alphaToCoverage = value;
+}
+
+export function blendAlpha(material: any, value: number): void {
+  material.blendAlpha = value;
+}
+
+export function blendColor(material: any, value: any): void { // ColorRepresentation
+  material.blendColor = value;
+}
+
+export function blendDst(material: any, value: any): void { // BlendingDstFactor
+  material.blendDst = value;
+}
+
+export function blendDstAlpha(material: any, value: number): void {
+  material.blendDstAlpha = value;
+}
+
+export function blendEquation(material: any, value: any): void { // BlendingEquation
+  material.blendEquation = value;
+}
+
+export function blendEquationAlpha(material: any, value: number): void {
+  material.blendEquationAlpha = value;
+}
+
+export function blending(material: any, value: any): void { // Blending
+  material.blending = value;
+}
+
+export function blendSrc(material: any, value: any): void { // BlendingSrcFactor | BlendingDstFactor
+  material.blendSrc = value;
+}
+
+export function blendSrcAlpha(material: any, value: number): void {
+  material.blendSrcAlpha = value;
+}
+
+export function clipIntersection(material: any, value: boolean): void {
+  material.clipIntersection = value;
+}
+
+export function clippingPlanes(material: any, value: any[]): void { // Plane[]
+  material.clippingPlanes = value;
+}
+
+export function clipShadows(material: any, value: boolean): void {
+  material.clipShadows = value;
+}
+
+export function colorWrite(material: any, value: boolean): void {
+  material.colorWrite = value;
+}
+
+export function defines(material: any, value: any): void {
+  material.defines = value;
+}
+
+export function depthFunc(material: any, value: any): void { // DepthModes
+  material.depthFunc = value;
+}
+
+export function depthTest(material: any, value: boolean): void {
+  material.depthTest = value;
+}
+
+export function depthWrite(material: any, value: boolean): void {
+  material.depthWrite = value;
+}
+
+export function name(material: any, value: string): void {
+  material.name = value;
+}
+
+export function opacity(material: any, value: number): void {
+  material.opacity = value;
+}
+
+export function polygonOffset(material: any, value: boolean): void {
+  material.polygonOffset = value;
+}
+
+export function polygonOffsetFactor(material: any, value: number): void {
+  material.polygonOffsetFactor = value;
+}
+
+export function polygonOffsetUnits(material: any, value: number): void {
+  material.polygonOffsetUnits = value;
+}
+
+export function precision(material: any, value: "highp" | "mediump" | "lowp" | null): void {
+  material.precision = value;
+}
+
+export function premultipliedAlpha(material: any, value: boolean): void {
+  material.premultipliedAlpha = value;
+}
+
+export function forceSinglePass(material: any, value: boolean): void {
+  material.forceSinglePass = value;
+}
+
+export function allowOverride(material: any, value: boolean): void {
+  material.allowOverride = value;
+}
+
+export function dithering(material: any, value: boolean): void {
+  material.dithering = value;
+}
+
+export function side(material: any, value: any): void { // Side
+  material.side = value;
+}
+
+export function shadowSide(material: any, value: any): void { // Side
+  material.shadowSide = value;
+}
+
+export function toneMapped(material: any, value: boolean): void {
+  material.toneMapped = value;
+}
+
+export function transparent(material: any, value: boolean): void {
+  material.transparent = value;
+}
+
+export function vertexColors(material: any, value: boolean): void {
+  material.vertexColors = value;
+}
+
+export function visible(material: any, value: boolean): void {
+  material.visible = value;
+}
+
+export function format(material: any, value: any): void { // PixelFormat
+  material.format = value;
+}
+
+export function stencilWrite(material: any, value: boolean): void {
+  material.stencilWrite = value;
+}
+
+export function stencilFunc(material: any, value: any): void { // StencilFunc
+  material.stencilFunc = value;
+}
+
+export function stencilRef(material: any, value: number): void {
+  material.stencilRef = value;
+}
+
+export function stencilWriteMask(material: any, value: number): void {
+  material.stencilWriteMask = value;
+}
+
+export function stencilFuncMask(material: any, value: number): void {
+  material.stencilFuncMask = value;
+}
+
+export function stencilFail(material: any, value: any): void { // StencilOp
+  material.stencilFail = value;
+}
+
+export function stencilZFail(material: any, value: any): void { // StencilOp
+  material.stencilZFail = value;
+}
+
+export function stencilZPass(material: any, value: any): void { // StencilOp
+  material.stencilZPass = value;
+}
+
+export function userData(material: any, value: Record<string, any>): void {
+  material.userData = value;
+}
+
+// JSON-specific properties
+
+export function color(material: any, value: number): void {
+  material.color = value;
+}
+
+export function roughness(material: any, value: number): void {
+  material.roughness = value;
+}
+
+export function metalness(material: any, value: number): void {
+  material.metalness = value;
+}
+
+export function sheen(material: any, value: number): void {
+  material.sheen = value;
+}
+
+export function sheenColor(material: any, value: number): void {
+  material.sheenColor = value;
+}
+
+export function sheenRoughness(material: any, value: number): void {
+  material.sheenRoughness = value;
+}
+
+export function emissive(material: any, value: number): void {
+  material.emissive = value;
+}
+
+export function emissiveIntensity(material: any, value: number): void {
+  material.emissiveIntensity = value;
+}
+
+export function specular(material: any, value: number): void {
+  material.specular = value;
+}
+
+export function specularIntensity(material: any, value: number): void {
+  material.specularIntensity = value;
+}
+
+export function specularColor(material: any, value: number): void {
+  material.specularColor = value;
+}
+
+export function shininess(material: any, value: number): void {
+  material.shininess = value;
+}
+
+export function clearcoat(material: any, value: number): void {
+  material.clearcoat = value;
+}
+
+export function clearcoatRoughness(material: any, value: number): void {
+  material.clearcoatRoughness = value;
+}
+
+export function clearcoatMap(material: any, value: string): void {
+  material.clearcoatMap = value;
+}
+
+export function clearcoatRoughnessMap(material: any, value: string): void {
+  material.clearcoatRoughnessMap = value;
+}
+
+export function clearcoatNormalMap(material: any, value: string): void {
+  material.clearcoatNormalMap = value;
+}
+
+export function clearcoatNormalScale(material: any, value: any): void { // Vector2Tuple
+  material.clearcoatNormalScale = value;
+}
+
+export function dispersion(material: any, value: number): void {
+  material.dispersion = value;
+}
+
+export function iridescence(material: any, value: number): void {
+  material.iridescence = value;
+}
+
+export function iridescenceIOR(material: any, value: number): void {
+  material.iridescenceIOR = value;
+}
+
+export function iridescenceThicknessRange(material: any, value: number): void {
+  material.iridescenceThicknessRange = value;
+}
+
+export function iridescenceMap(material: any, value: string): void {
+  material.iridescenceMap = value;
+}
+
+export function iridescenceThicknessMap(material: any, value: string): void {
+  material.iridescenceThicknessMap = value;
+}
+
+export function anisotropy(material: any, value: number): void {
+  material.anisotropy = value;
+}
+
+export function anisotropyRotation(material: any, value: number): void {
+  material.anisotropyRotation = value;
+}
+
+export function anisotropyMap(material: any, value: string): void {
+  material.anisotropyMap = value;
+}
+
+export function map(material: any, value: string): void {
+  material.map = value;
+}
+
+export function matcap(material: any, value: string): void {
+  material.matcap = value;
+}
+
+export function alphaMap(material: any, value: string): void {
+  material.alphaMap = value;
+}
+
+export function lightMap(material: any, value: string): void {
+  material.lightMap = value;
+}
+
+export function lightMapIntensity(material: any, value: number): void {
+  material.lightMapIntensity = value;
+}
+
+export function aoMap(material: any, value: string): void {
+  material.aoMap = value;
+}
+
+export function aoMapIntensity(material: any, value: number): void {
+  material.aoMapIntensity = value;
+}
+
+export function bumpMap(material: any, value: string): void {
+  material.bumpMap = value;
+}
+
+export function bumpScale(material: any, value: number): void {
+  material.bumpScale = value;
+}
+
+export function normalMap(material: any, value: string): void {
+  material.normalMap = value;
+}
+
+export function normalMapType(material: any, value: any): void { // NormalMapTypes
+  material.normalMapType = value;
+}
+
+export function normalScale(material: any, value: any): void { // Vector2Tuple
+  material.normalScale = value;
+}
+
+export function displacementMap(material: any, value: string): void {
+  material.displacementMap = value;
+}
+
+export function displacementScale(material: any, value: number): void {
+  material.displacementScale = value;
+}
+
+export function displacementBias(material: any, value: number): void {
+  material.displacementBias = value;
+}
+
+export function roughnessMap(material: any, value: string): void {
+  material.roughnessMap = value;
+}
+
+export function metalnessMap(material: any, value: string): void {
+  material.metalnessMap = value;
+}
+
+export function emissiveMap(material: any, value: string): void {
+  material.emissiveMap = value;
+}
+
+export function specularMap(material: any, value: string): void {
+  material.specularMap = value;
+}
+
+export function specularIntensityMap(material: any, value: string): void {
+  material.specularIntensityMap = value;
+}
+
+export function specularColorMap(material: any, value: string): void {
+  material.specularColorMap = value;
+}
+
+export function envMap(material: any, value: string): void {
+  material.envMap = value;
+}
+
+export function combine(material: any, value: any): void { // Combine
+  material.combine = value;
+}
+
+export function envMapRotation(material: any, value: any): void { // EulerTuple
+  material.envMapRotation = value;
+}
+
+export function envMapIntensity(material: any, value: number): void {
+  material.envMapIntensity = value;
+}
+
+export function reflectivity(material: any, value: number): void {
+  material.reflectivity = value;
+}
+
+export function refractionRatio(material: any, value: number): void {
+  material.refractionRatio = value;
+}
+
+export function gradientMap(material: any, value: string): void {
+  material.gradientMap = value;
+}
+
+export function transmission(material: any, value: number): void {
+  material.transmission = value;
+}
+
+export function transmissionMap(material: any, value: string): void {
+  material.transmissionMap = value;
+}
+
+export function thickness(material: any, value: number): void {
+  material.thickness = value;
+}
+
+export function thicknessMap(material: any, value: string): void {
+  material.thicknessMap = value;
+}
+
+export function attenuationDistance(material: any, value: number): void {
+  material.attenuationDistance = value;
+}
+
+export function attenuationColor(material: any, value: number): void {
+  material.attenuationColor = value;
+}
+
+export function size(material: any, value: number): void {
+  material.size = value;
+}
+
+export function shadowSideNumber(material: any, value: number): void {
+  material.shadowSide = value;
+}
+
+export function sizeAttenuation(material: any, value: boolean): void {
+  material.sizeAttenuation = value;
+}
+
+export function rotation(material: any, value: number): void {
+  material.rotation = value;
+}
+
+export function linewidth(material: any, value: number): void {
+  material.linewidth = value;
+}
+
+export function dashSize(material: any, value: number): void {
+  material.dashSize = value;
+}
+
+export function gapSize(material: any, value: number): void {
+  material.gapSize = value;
+}
+
+export function scale(material: any, value: number): void {
+  material.scale = value;
+}
+
+export function wireframe(material: any, value: boolean): void {
+  material.wireframe = value;
+}
+
+export function wireframeLinewidth(material: any, value: number): void {
+  material.wireframeLinewidth = value;
+}
+
+export function wireframeLinecap(material: any, value: string): void {
+  material.wireframeLinecap = value;
+}
+
+export function wireframeLinejoin(material: any, value: string): void {
+  material.wireframeLinejoin = value;
+}
+
+export function flatShading(material: any, value: boolean): void {
+  material.flatShading = value;
+}
+
+export function fog(material: any, value: boolean): void {
+  material.fog = value;
 }
